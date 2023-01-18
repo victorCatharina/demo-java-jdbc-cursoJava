1f1af93bca570c0e8c9f645d7d8e57392f4d851a
Victor Silva <matheusvictor5019@gmail.com>
2023-01-18 20:43:34 -0300

    Configured database connection

---
+++ src/db/DbIntegrityException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntegrityException extends RuntimeException {
+
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;
+
+    public DbIntegrityException(String msg) {
+        super(msg);
+    }
+}
