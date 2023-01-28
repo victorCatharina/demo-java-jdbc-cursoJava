a86868a1bfe933d7cd1c92745b029eddc5288387
Victor Silva <matheusvictor5019@gmail.com>
2023-01-28 11:23:50 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department department = new Department(1, "Books");
+        System.out.println(department);
+    }
+}
